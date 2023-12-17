c86ad4b03cd6766f8c3c11a2af8c59edf0ee57e8
Avishkar <avishkar2108@gmail.com>
2023-12-17 20:35:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Image-Image translation using Pix2Pix
+
+## Download data [here](https://sd.keepcalms.com/i/yo-you-just-got-played-1.png)
+
+```git clone <this repo>```
---
+++ discriminator_model.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, stride=2):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 4, stride, bias=False, padding_mode="reflect"),
+            nn.BatchNorm2d(out_channel),
+            nn.LeakyReLU(0.2)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+# we send in input & output imgs together , hence in_channels*2 in initial block
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]): #
+        super().__init__()
+        # initial block
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels*2, features[0], kernel_size=4, stride=2, padding=1, padding_mode="reflect"),
+            nn.LeakyReLU(0.2),
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                CNNBlock(in_channels, feature, stride=1 if feature == features[-1] else 2),
+            )
+            in_channels = feature
+
+        self.model = nn.Sequential(*layers)  # unpack all stuff of the list
+
+    def forward(self, x, y):
+        x = torch.cat([x,y], dim=1)  # along rgb channels
+        x = self.initial(x)
+        return self.model(x)
+
+
+def test():
+    x = torch.randn((1, 3, 1080, 810))
+    y = torch.randn((1, 3, 1080, 810))
+    model = Discriminator()
+    preds = model(x, y)
+    print(preds.shape)
+
+
+if __name__ == "__main__":
+    test()
